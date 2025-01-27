ac07c66ea50f95f35ea0143d53ff7b8c19e1dcad
KeiAxiro <makfaruk6@gmail.com>
2025-01-27 21:48:34 +0700

    Update Vercel configuration and improve views directory path handling

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,6 @@ import morgan from "morgan";
 const app: Application = express();
 const PORT = 3000;
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 import apiRouter from "./routes/api/indexApi.js";
 import indexRouter from "./routes/indexRouter.js";
 
@@ -15,8 +13,8 @@ app.use(morgan("dev"));
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
+app.set("views", path.join(`${__dirname}/views`));
+console.log("Views directory:", path.join(__dirname, "views"));
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
